685c7d84434569e448580bdbc6492c1b784b60d8
cramblda <david@functionalchaos.net>
2017-05-22 22:29:33 -0700

    Add instagram feed

---
+++ source/js/instagram-feed.js
@@ -0,0 +1,8 @@
+
+    var userFeed = new Instafeed({
+        get: 'user',
+        userId: '2007402176',
+        accessToken: '2007402176.1677ed0.68c78b6fda7544349f562a84676bc26d',
+        resolution: 'low_resolution'
+    });
+    userFeed.run();
