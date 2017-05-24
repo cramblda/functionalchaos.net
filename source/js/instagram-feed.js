ded411960a93cbc181a91bbea26e8c408a77e1b9
David <david@fc-mobile1.functionalchaos.net>
2017-05-23 23:02:29 -0700

    Improvments to instafeed layout

--- source/js/instagram-feed.js
+++ source/js/instagram-feed.js
@@ -1,8 +1,11 @@
 
-    var userFeed = new Instafeed({
-        get: 'user',
-        userId: '2007402176',
-        accessToken: '2007402176.1677ed0.68c78b6fda7544349f562a84676bc26d',
-        resolution: 'low_resolution'
-    });
-    userFeed.run();
+var userFeed = new Instafeed({
+    get: 'user',
+    userId: '2007402176',
+    accessToken: '2007402176.1677ed0.68c78b6fda7544349f562a84676bc26d',
+    resolution: 'standard_resolution',
+    limit: 6,
+    sortBy: 'most-recent',
+    template: '<div class="col-xs-4 col-md-2 instafeed-col"><a href="{{link}}"><img class="instafeed" src="{{image}}" /></a></div>'
+});
+userFeed.run();
