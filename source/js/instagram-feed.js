8b23e3b1ed343d7794c55a2abbdb98dcf78923ee
cramblda <david@functionalchaos.net>
2022-09-11 16:31:07 -0700

    revert to orig instagram feed config

--- source/js/instagram-feed.js
+++ source/js/instagram-feed.js
@@ -1,5 +1,5 @@
 const userFeed = new Instafeed({
-  accessToken: '[token-here]',
+  accessToken: InstagramToken,
   limit: 6,
   template: '<div class="col-xs-4 col-md-2 instafeed-col"><a href="{{link}}" target="_blank"><img class="instafeed" src="{{image}}" /></a></div>'
 });
